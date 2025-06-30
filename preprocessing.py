5422a4961edd67143925ee882ad6a1bc2e19cf1c
YATHISRI <yathisrikoyya@gmail.com>
2025-06-30 19:27:40 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,8 @@
 # Dummy file for preprocessing.py
+import cv2
+
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (224, 224))
+    img = img / 255.0
+    return img
