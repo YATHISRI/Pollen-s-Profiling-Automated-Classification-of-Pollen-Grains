c09d0a0f6b64a412be469e398cfacbab150cd3c7
YATHISRI <yathisrikoyya@gmail.com>
2025-06-30 19:26:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Dummy file for main.py
+from flask import Flask, render_template, request
+from utils.model_utils import load_model_and_predict
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        image = request.files['image']
+        prediction = load_model_and_predict(image)
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
